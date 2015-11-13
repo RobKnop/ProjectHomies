6e9c3a04cbab2b8b371441e5e8674652348b52ec
dustinchhum <shabo9810@gmail.com>
2015-11-13 11:36:35 -0800

    ListView & CustomList Adapters
    Prototype!

---
+++ app/src/main/java/edu/csulb/android/projecthomies/CustomListHomePage.java
@@ -0,0 +1,31 @@
+package edu.csulb.android.projecthomies;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class CustomListHomePage extends ArrayAdapter<String> {
+    private final Activity context;
+    private final ArrayList<String> web;
+
+    public CustomListHomePage(Activity context, ArrayList<String> web) {
+        super(context, R.layout.list_view_homepage, web);
+        this.context = context;
+        this.web = web;
+    }
+
+    public View getView(int position, View view, ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        View rowView  = inflater.inflate(R.layout.list_view_homepage, null, true);
+        TextView txtTitle = (TextView)rowView.findViewById(R.id.txt);
+
+        txtTitle.setText(web.get(position));
+
+        return rowView;
+    }
+}
