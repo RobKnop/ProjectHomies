d3a6af106bc1cff5dd37e053ce6f19a21ab7c000
emilyahoang <emilyanhhoang@gmail.com>
2015-12-03 17:06:12 -0800

    Non Functioning Reminder Examples added to Reminder ListView

--- app/src/main/java/edu/csulb/android/projecthomies/AddReminder.java
+++ app/src/main/java/edu/csulb/android/projecthomies/AddReminder.java
@@ -2,13 +2,58 @@ package edu.csulb.android.projecthomies;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+import java.util.Date;
+
 
 public class AddReminder extends AppCompatActivity
 {
+    private EditText alarmName;
+    private DatePicker dPicker;
+    private TimePicker tPicker;
+
+    private Date date;
+    private String time;
+    private String alarm;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_reminder);
+        findViews();
+    }
+
+    // Save an alarm
+    public void createAlarm(View v)
+    {
+        alarm = alarmName.getText().toString();
+        date = getDateFromDatePicket(dPicker);
+
+    }
+
+    // Find all the GUI elements by views
+    private void findViews()
+    {
+        alarmName = (EditText) findViewById(R.id.editText_alarmName);
+        dPicker = (DatePicker) findViewById(R.id.datePicker);
+        tPicker = (TimePicker) findViewById(R.id.timePicker);
+    }
+
+    public static Date getDateFromDatePicket(DatePicker datePicker)
+    {
+        int day = datePicker.getDayOfMonth();
+        int month = datePicker.getMonth();
+        int year =  datePicker.getYear();
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month, day);
+
+        return calendar.getTime();
     }
 }
--- app/src/main/java/edu/csulb/android/projecthomies/Reminders.java
+++ app/src/main/java/edu/csulb/android/projecthomies/Reminders.java
@@ -1,14 +1,25 @@
 package edu.csulb.android.projecthomies;
 
+import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 public class Reminders extends AppCompatActivity
 {
+    ArrayList<String> remindersList = new ArrayList<String>();
+    ArrayAdapter<String> adapter;
+
     private View fabAction;
 
     @Override
@@ -18,14 +29,27 @@ public class Reminders extends AppCompatActivity
         setContentView(R.layout.activity_reminders);
 
         fabAction = findViewById(R.id.fabAddReminder);
-        fabAction.setOnClickListener(new View.OnClickListener()
-        {
+        fabAction.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 Intent i = new Intent(Reminders.this, AddReminder.class);
                 startActivity(i);
             }
         });
+
+        initList();
+        ListView lv = (ListView) findViewById(R.id.listView);
+        adapter=new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                remindersList);
+        lv.setAdapter(adapter);
+    }
+
+    private void initList()
+    {
+        remindersList.add("Meet Up Reminder");
+        remindersList.add("Call Your Friend Reminder");
+        remindersList.add("Dinner Reminder");
+        remindersList.add("Party Reminder");
     }
 }
